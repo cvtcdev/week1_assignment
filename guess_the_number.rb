235d8fd3e7f24d8a3250c0994ec52e6bdc2e3b43
Edward Dumholt <edumholt@gmail.com>
2015-07-08 11:20:04 -0500

    Completed, with no global variables and refactored.

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,13 +1,7 @@
-difficulty_level = 0
-difficulty_range = 1..3
-guesses = 1
-max_possible_num = 0
-random_num = Random.new
-play_again = 'y'
-
-while play_again == 'y'
-
-  puts "Let's play Guess the Number"
+def get_number_to_guess
+  difficulty_level = 0
+  difficulty_range = 1..3
+  max_possible_num = 0
 
   until difficulty_range.include?(difficulty_level)
     puts 'Please select a level of difficulty (1, 2, or 3):'
@@ -23,24 +17,12 @@ while play_again == 'y'
       max_possible_num = 1000
   end
 
-  number_to_guess = random_num.rand(1..max_possible_num)
+  random_num = Random.new
+  random_num.rand(1..max_possible_num)
 
-  puts "I have my number. What's your guess? "
-  player_guess = gets.chop.to_i
+end
 
-  until player_guess == number_to_guess
-    if player_guess == 0
-      puts 'Please enter only numbers. Try again.'
-    elsif player_guess > number_to_guess
-      puts 'Too high. Guess again: '
-    else
-      puts 'Too low. Guess again: '
-    end
-
-    player_guess = gets.chop.to_i
-    guesses += 1
-  end
-
+def show_results(guesses, number_to_guess)
   result_phrase = if guesses == 1
                     "You're a mind reader!"
                   elsif guesses >= 2 && guesses <= 3
@@ -54,5 +36,34 @@ while play_again == 'y'
   puts "That's right! The number is #{number_to_guess}"
   puts "You got it in #{guesses} guesses! " + result_phrase
   puts 'Play again? (y/n)'
-  play_again = gets.chop
 end
+
+def main
+
+  play_again = 'y'
+  while play_again == 'y'
+    guesses = 1
+    puts "Let's play Guess the Number"
+    number_to_guess = get_number_to_guess
+    puts "I have my number. What's your guess? "
+    player_guess = gets.chop.to_i
+
+    until player_guess == number_to_guess
+      if player_guess <= 0
+        puts 'Please enter only positive numbers. Try again.'
+      elsif player_guess > number_to_guess
+        puts 'Too high. Guess again: '
+      else
+        puts 'Too low. Guess again: '
+      end
+
+      player_guess = gets.chop.to_i
+      guesses += 1
+    end
+
+    show_results(guesses, number_to_guess)
+    play_again = gets.chop
+  end
+end
+
+main
